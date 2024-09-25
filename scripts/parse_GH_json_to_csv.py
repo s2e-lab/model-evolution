fbaa19026c54de3860ce1b963adebe829e46db65
kaiadamian <kaiagabsdamian@gmail.com>
2024-09-24 23:54:52 -0400

    added github json to csv script and .csv data

---
+++ scripts/parse_GH_json_to_csv.py
@@ -0,0 +1,17 @@
+import json
+import csv
+
+json_input_file = '../data/GH_data_safetensor.json'
+csv_output_file = '../results/GH_data_safetensor.csv'
+
+with open (json_input_file) as f:
+    data = json.load(f)
+
+with open (csv_output_file, "w") as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(["source", "url", "json content"])
+
+    for entry in data:
+        csv_writer.writerow(["GitHub", entry["url"], json.dumps(entry)])
+
+print(f"{json_input_file} converted ti .csv and written to {csv_output_file}")
