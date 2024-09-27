ddc5fa685e7049c0a8dd2d30bc2af22135dd17f6
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-09-27 14:53:45 -0400

    screipts with ranges

---
+++ scripts/andrew.sh
@@ -0,0 +1,5 @@
+python get_repos_history.py 517 600
+python get_repos_history.py 600 700
+python get_repos_history.py 700 800
+python get_repos_history.py 800 900
+python get_repos_history.py 900 1035
---
+++ scripts/kaia.sh
@@ -0,0 +1,5 @@
+python get_repos_history.py 0 100
+python get_repos_history.py 100 200
+python get_repos_history.py 200 300
+python get_repos_history.py 300 400
+python get_repos_history.py 400 517
