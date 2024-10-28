78a3779a86aef20113e72031ee9f95d1b0306570
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-10-27 21:12:49 -0400

    Computed cosine similarity for GH and SO posts -- adjusted script to set is_true_positive to 0 for least relevant rows

--- scripts/analyze_devs_discussion.py
+++ scripts/analyze_devs_discussion.py
@@ -22,7 +22,7 @@ def compute_similarity(query: str, df: pd.DataFrame, columns: []):
 
 
 if __name__ == '__main__':
-    # sys.argv.append("../data/GH_data_safetensor.csv")
+
     if len(sys.argv) < 2:
         print("Usage: python analyze_devs_discussion.py <filename>")
         sys.exit(1)
@@ -41,19 +41,29 @@ if __name__ == '__main__':
     # format the dataframe columns
     df = df[['cosine_similarity', 'source', 'json_content']]
     df = df.rename(columns={'json_content': 'content'})
+    # make index start at 1
+    df.index = df.index + 1
     df.rename_axis('id', inplace=True)
     df['is_true_positive'] = None
     df['comments'] = None
 
+    df = df[[ 'source', 'content', 'is_true_positive', 'comments','cosine_similarity']]
+
     # calculate sample size
     sample_size = calculate_sample_size(len(df), 0.95, 0.05)
 
-    # make index start at 1
-    df.index = df.index + 1
+    # set is_true_positive to False for the least relevant rows
+    count = 0
+    for index, row in df.iterrows():
+        count += 1
+        if count > sample_size:
+            df.at[index, 'is_true_positive'] = 0
 
-    # save only the first sample_size rows
+
+    # save the dataframe to a new file
     output_filename = filename.replace('.csv', '_sorted.csv')
     df.to_csv(output_filename, index=True)
 
     print(f"Samples saved to {output_filename}")
     print(f"Sample size: {sample_size}")
+
