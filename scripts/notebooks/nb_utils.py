a55ee65db9bede9cc1750dcfa87bdf52d2139310
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2024-10-31 20:26:17 -0400

    Added data for bot

--- scripts/notebooks/nb_utils.py
+++ scripts/notebooks/nb_utils.py
@@ -1,10 +1,6 @@
-
-
-#%%
 from pathlib import Path
 
 import pandas as pd
-
 
 # Reference date when safetensors was released
 SAFETENSORS_RELEASE_DATE = pd.to_datetime("2022-09-23")
@@ -18,17 +14,19 @@ def read_commits():
     for index, row in df_commits.iterrows():
         info_to_date[row['commit_hash'] + row['repo_url']] = row
     # grabs the date from df2 and adds it to df based on commit_hash and repo_url matching
+    df['date'] = ""
     for index, row in df.iterrows():
         commit_hash = row['commit_hash']
         repo_url = row['repo_url']
-        row['date'] = info_to_date[commit_hash + repo_url]['date']
-        row['message'] = info_to_date[commit_hash + repo_url]['message']
+        df.loc[index, 'date'] = info_to_date[commit_hash + repo_url]['date']
+        df.loc[index, 'message'] = info_to_date[commit_hash + repo_url]['message']
 
     df['date'] = pd.to_datetime(df['date'])
 
     # Calculate elapsed days since reference date
     df['elapsed_days'] = (df['date'] - SAFETENSORS_RELEASE_DATE).dt.days
     return df
+
 
 def get_safetensors_releases():
     """
@@ -43,7 +41,8 @@ def get_safetensors_releases():
     df_releases = df_releases[~df_releases['tag'].str.contains('rc')]
 
     # for releases on the same date, prefer the one that does not start with python-
-    df_releases = df_releases.sort_values(['date', 'tag'], ascending=[True, False]).drop_duplicates('date', keep='first')
+    df_releases = df_releases.sort_values(['date', 'tag'], ascending=[True, False]).drop_duplicates('date',
+                                                                                                    keep='first')
 
     # remove the python- prefix from the tag
     df_releases['tag'] = df_releases['tag'].str.replace('python-', '')
