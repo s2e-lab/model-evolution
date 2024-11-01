611e2c4d21b7779841dadae0e1fe209332290ae9
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2024-10-31 22:15:36 -0400

    Added crawler for SFConvertBot discussion

--- scripts/get_sfconvert_prs.py
+++ scripts/get_sfconvert_prs.py
@@ -2,13 +2,14 @@
 This script is used to check the status of the PRs that were created by the sfconvertbot.
 @Author: Joanna C. S. Santos
 """
+import math
 from pathlib import Path
 
 import requests
 from datasets import load_dataset
 from requests import Response
 from tqdm import tqdm
-
+import pandas as pd
 
 def get_pr_status(response: Response) -> str:
     """
@@ -32,19 +33,23 @@ def get_pr_status(response: Response) -> str:
 
 
 if __name__ == '__main__':
-    ds = load_dataset("safetensors/conversions")['train']
-    # convert dataset to dataframe
-    df_sfconvert = ds.to_pandas()
-    df_sfconvert['time'].min(), df_sfconvert['time'].max()
+    # ds = load_dataset("safetensors/conversions")['train']
+    # # convert dataset to dataframe
+    # df_sfconvert = ds.to_pandas()
+    # df_sfconvert['time'].min(), df_sfconvert['time'].max()
 
+    df_sfconvert = pd.read_csv(Path('../results/sfconvertbot_merge_status.csv'))
+    # convert nan to None
+    df_sfconvert['merged'] = df_sfconvert['merged'].apply(lambda x: x if str(x) != "nan" else None)
     # iterate over dataframe to check whether the PRs were merged
-    for i, row in tqdm(df_sfconvert.iterrows()):
+    for i, row in tqdm(df_sfconvert.iterrows(), total=len(df_sfconvert)):
+        if row['merged'] != None or i < 8000: continue
         pr_url = row['pr_url']
         # make an HTTP request
         response = requests.get(pr_url)
         df_sfconvert.loc[i, 'merged'] = get_pr_status(response)
         # SAVES THE DATAFRAME EVERY 500 ITERATIONS
         if i != 0 and i % 500 == 0:
-            df_sfconvert.to_csv(Path(f'../../results/sfconvertbot_merge_status_{i}.csv'), index=False)
+            df_sfconvert.to_csv(Path(f'../results/sfconvertbot_merge_status_{i}.csv'), index=False)
 
-    df_sfconvert.to_csv(Path('../../results/sfconvertbot_merge_status.csv'), index=False)
+    df_sfconvert.to_csv(Path('../results/sfconvertbot_merge_status.csv'), index=False)
