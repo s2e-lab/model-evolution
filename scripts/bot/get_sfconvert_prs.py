5ba74ce763452cd1e0f51f5bbba320c8a1870b53
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-11-02 23:59:02 -0400

    adjusted script to get PR metadata

--- scripts/bot/get_sfconvert_prs.py
+++ scripts/bot/get_sfconvert_prs.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 
-from bot_utils import extract_discussion_metadata
+from scripts.bot.bot_utils import extract_discussion_metadata
 
 if __name__ == '__main__':
     # load prior results from data/bot_cache
@@ -21,7 +21,6 @@ if __name__ == '__main__':
             pr_url = row['pr_url'].split("#")[0]
             if row['model_id']:
                 cache[pr_url] = row
-
 
     df = pd.read_csv(Path("../../data/sfconvertbot_pr_urls.csv"))
     out_file_prefix = '../../data/sfconvertbot_pr_metadata'
@@ -37,22 +36,24 @@ if __name__ == '__main__':
             df.loc[i, 'time'] = row['time']
             continue
         else:
-            print("Not in cache: " + pr_url)
-        continue
-        events, header = extract_discussion_metadata(pr_url)
-        df.loc[i, 'discussion_metadata'] = events
-        df.loc[i, 'header_metadata'] = header
-        # parse the header as JSON
-        json_header = json.loads(header) if header and header.startswith('{') else None
-
-        df.loc[i, 'model_id'] = json_header['discussion']['repo']['name'] if json_header else None
-        df.loc[i, 'time'] = json_header['discussion']['createdAt'] if json_header else None
+            events, header = extract_discussion_metadata(pr_url)
+            df.loc[i, 'discussion_metadata'] = events
+            df.loc[i, 'header_metadata'] = header
+            # parse the header as JSON
+            json_header = json.loads(header) if header and header.startswith('{') else None
+            df.loc[i, 'model_id'] = json_header['discussion']['repo']['name'] if json_header else None
+            df.loc[i, 'time'] = json_header['discussion']['createdAt'] if json_header else None
 
         # SAVES THE DATAFRAME EVERY 500 ITERATIONS
         if i != 0 and i % 500 == 0:
             df.to_csv(Path(out_file_prefix + f'_{i}.csv'), index=False)
+            # delete prior checkpoint file
+            if Path(out_file_prefix + f'_{i - 500}.csv').exists():
+                os.remove(Path(out_file_prefix + f'_{i - 500}.csv'))
 
     df.to_csv(Path(out_file_prefix + '.csv'), index=False)
-    # delete the checkpoint files
+    # delete the  checkpoint files
     for i in range(500, len(df), 500):
-        os.remove(Path(out_file_prefix + f'_{i}.csv'))
+        check_file = Path(out_file_prefix + f'_{i}.csv')
+        if check_file.exists():
+            os.remove(check_file)
