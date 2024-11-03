027c4b3c0e7e6070ed2e760254ace77a7c415227
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-11-02 23:09:21 -0400

    script to get pr data from cache

--- scripts/bot/get_sfconvert_prs.py
+++ scripts/bot/get_sfconvert_prs.py
@@ -2,22 +2,43 @@
 This script is used to check the status of the PRs that were created by the sfconvertbot.
 @Author: Joanna C. S. Santos
 """
+import json
 import os
 from pathlib import Path
 
 import pandas as pd
 from tqdm import tqdm
-import json
 
 from bot_utils import extract_discussion_metadata
 
 if __name__ == '__main__':
+    # load prior results from data/bot_cache
+    prior_files = [f for f in os.listdir(Path('../../data/bot_cache')) if f.endswith('.csv')]
+    cache = dict()  # key = pr_url, value = row
+    for f in prior_files:
+        df = pd.read_csv(os.path.join('../../data/bot_cache', f)).fillna("")
+        for index, row in df.iterrows():
+            pr_url = row['pr_url'].split("#")[0]
+            if row['model_id']:
+                cache[pr_url] = row
+
+
     df = pd.read_csv(Path("../../data/sfconvertbot_pr_urls.csv"))
     out_file_prefix = '../../data/sfconvertbot_pr_metadata'
 
     # iterate over dataframe to check whether the PRs were merged
     for i, row in tqdm(df.iterrows(), total=len(df)):
-        pr_url = row['pr_url']
+        pr_url = row['pr_url'].split("#")[0]
+        if pr_url in cache:
+            row = cache[pr_url]
+            df.loc[i, 'discussion_metadata'] = row['discussion_metadata']
+            df.loc[i, 'header_metadata'] = row['header_metadata']
+            df.loc[i, 'model_id'] = row['model_id']
+            df.loc[i, 'time'] = row['time']
+            continue
+        else:
+            print("Not in cache: " + pr_url)
+        continue
         events, header = extract_discussion_metadata(pr_url)
         df.loc[i, 'discussion_metadata'] = events
         df.loc[i, 'header_metadata'] = header
@@ -35,4 +56,3 @@ if __name__ == '__main__':
     # delete the checkpoint files
     for i in range(500, len(df), 500):
         os.remove(Path(out_file_prefix + f'_{i}.csv'))
-
