92e387595220264875dd3b70b9d4249bfbd4ec35
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2024-11-03 09:45:30 -0500

    re-rprocess RQ1 and RQ2 usign latest data

--- scripts/notebooks/nb_utils.py
+++ scripts/notebooks/nb_utils.py
@@ -4,26 +4,19 @@ import pandas as pd
 
 # Reference date when safetensors was released
 SAFETENSORS_RELEASE_DATE = pd.to_datetime("2022-09-23")
-
+# Data and results directories
+DATA_DIR = Path('../../data')
+RESULTS_DIR = Path('../../results')
 
 def read_commits():
-    df = pd.read_csv(Path('../../data/fixed_repository_evolution_commits_0_5014.csv'))
-    df_commits = pd.read_csv(Path('../../data/huggingface_sort_by_createdAt_top996939_commits_0_1035.csv'))
-    # local cache: commit_hash + repo_url -> date
-    info_to_date = dict()
-    for index, row in df_commits.iterrows():
-        info_to_date[row['commit_hash'] + row['repo_url']] = row
-    # grabs the date from df2 and adds it to df based on commit_hash and repo_url matching
-    df['date'] = ""
-    for index, row in df.iterrows():
-        commit_hash = row['commit_hash']
-        repo_url = row['repo_url']
-        df.loc[index, 'date'] = info_to_date[commit_hash + repo_url]['date']
-        df.loc[index, 'message'] = info_to_date[commit_hash + repo_url]['message']
-
+    """
+    Read the commits from the repository evolution dataset.
+    :return:
+    """
+    df = pd.read_csv(DATA_DIR / 'repository_evolution_commits_0_5014.csv')
+    # ensure date is in datetime format
     df['date'] = pd.to_datetime(df['date'])
-
-    # Calculate elapsed days since reference date
+    # Calculate elapsed days since reference date (safetensors first release)
     df['elapsed_days'] = (df['date'] - SAFETENSORS_RELEASE_DATE).dt.days
     return df
 
@@ -34,7 +27,7 @@ def get_safetensors_releases():
     :return: a dataframe with the dates of the releases in days since the first release and their corresponding labels
     """
     # Dates of the releases in days since the first release and their corresponding labels
-    df_releases = pd.read_csv(Path('../../data/safetensors_tags.csv'))
+    df_releases = pd.read_csv(DATA_DIR / 'safetensors_tags.csv')
     df_releases['date'] = pd.to_datetime(df_releases['date'])
     df_releases['elapsed_days'] = (df_releases['date'] - SAFETENSORS_RELEASE_DATE).dt.days
     # remove rc releases
