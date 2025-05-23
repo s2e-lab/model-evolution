4812a9422fa8bcb3bddd61b4aa35727f7d402714
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-05-23 16:44:11 -0400

    Added TQDM to track status of retrieval of further metadata

--- scripts/get_models.py
+++ scripts/get_models.py
@@ -6,39 +6,42 @@ The models are sorted by the number of likes or downloads. The JSON file is save
 """
 import json
 import zipfile
+import time
 from pathlib import Path
 
 from analyticaml.model_download import get_models_metadata
 from huggingface_hub import HfApi
+from tqdm import tqdm
 
 if __name__ == '__main__':
     print("Getting models from Hugging Face API...")
     # Configure what  models to get from the Hugging Face API
-    total = None                    # if None, it will retrieve all models
+    total = None  # if None, it will retrieve all models
     sorting_criteria = "createdAt"  # "downloads" "likes"
-    full = False                    # whether to get the full model information (true) or not (false)
-    sort_direction = True           # True for ascending, False for descending
+    full = False  # whether to get the full model information (true) or not (false)
+    sort_direction = True  # True for ascending, False for descending
 
     # Retrieve the models
+    start = time.perf_counter()
     models = get_models_metadata(sorting_criteria, total, full, sort_direction)
-    print("Downloaded", len(models), "models")
+    print(f"Downloaded {len(models)} models in {(time.perf_counter() - start):.4f} seconds")
 
     # Parse the retrieved metadata
     results = []
 
     # Used to get the model information in more details (e.g. siblings' size and total repo size)
     api = HfApi()
-    for model in models:
+    for model in tqdm(models):
         # skips models created after 2024
         if model.created_at.year > 2024: continue
-        repo_files = []
 
-        if model.gated:
+        if model.gated:  # if it is gated, don't try to get the model info (we cant anyway!)
             results.append(vars(model))
-        else:
+        else:  # if it is not gated, get the model info to include repo size and files' sizes
             model_info = api.model_info(model.id, revision=model.sha, files_metadata=True)
             results.append(vars(model_info))
             # Get the model files
+            repo_files = []
             if model_info.siblings:
                 for sibling in model_info.siblings:
                     repo_file = vars(sibling)
