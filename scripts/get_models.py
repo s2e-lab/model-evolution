30eaf6b7e18be3f1d75e7cde31a7fc5506832764
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-05-24 00:19:02 -0400

    Added a safeguard so the script would not crash halfway through

--- scripts/get_models.py
+++ scripts/get_models.py
@@ -15,6 +15,24 @@ from tqdm import tqdm
 
 from select_models import FIRST_DAY_OF_2024
 
+
+def save(models_list: list, out_file: Path) -> None:
+    """
+    This function saves the models metadata to a JSON file and compresses it.
+    :param models_list:  list of models metadata
+    :param out_file: path to the output file
+    """
+    print(f"Saving {len(models_list)} models metadata")
+    with open(out_file, "w") as outfile:
+        outfile.write(json.dumps(models_list, indent=2, default=str))
+    # Compress the file
+    with zipfile.ZipFile(out_file.with_suffix(".json.zip"), 'w', compression=zipfile.ZIP_DEFLATED) as zip_ref:
+        zip_ref.write(out_file, arcname=out_file.name)
+
+    # Delete the uncompressed file
+    out_file.unlink()
+
+
 if __name__ == '__main__':
     print("Getting models from Hugging Face API...")
     # Configure what  models to get from the Hugging Face API
@@ -23,7 +41,7 @@ if __name__ == '__main__':
     full = True  # whether to get the full model information (true) or not (false)
     sort_direction = True  # True for ascending, False for descending
 
-    # Retrieve the modelsx
+    # Retrieve the models
     start = time.perf_counter()
     models = get_models_metadata(sorting_criteria, total, full, sort_direction)
     models = [m for m in models if m.created_at.year <= 2024]  # skips models created after 2024
@@ -33,12 +51,18 @@ if __name__ == '__main__':
     results = []
     api = HfApi()
     print("Parsing models metadata...")
+    save_at = 10000
+    output_file = Path(f"../data/hf_sort_by_{sorting_criteria}_top{len(models)}.json")
     for model in tqdm(models):
         # if it is gated, or it does not have recent changes, then don't try to get the model info
         if model.gated or model.last_modified < FIRST_DAY_OF_2024:
             model_metadata = model
-        else: # otherwise, get the model metadata from the Hugging Face API
-            model_metadata = api.model_info(model.id, revision=model.sha, files_metadata=False)
+        else:  # otherwise, get the model metadata from the Hugging Face API
+            try:
+                model_metadata = api.model_info(model.id, revision=model.sha, files_metadata=False)
+            except Exception as e:
+                print(f"Error retrieving model metadata for {model.id}: {e}")
+                model_metadata = model
         results.append(vars(model_metadata))
         # Get the model files
         repo_files = []
@@ -49,19 +73,10 @@ if __name__ == '__main__':
                 repo_files.append(repo_file)
 
         results[-1]["siblings"] = repo_files
+        if len(results) % save_at == 0:
+            save(results, output_file)
 
     # Save the results as a zip file
-    print("Saving  results")
-
-    output_file = Path(f"../data/hf_sort_by_{sorting_criteria}_top{len(results)}.json")
-    with open(output_file, "w") as outfile:
-        outfile.write(json.dumps(results, indent=2, default=str))
-
-    # Compress the file
-    with zipfile.ZipFile(output_file.with_suffix(".json.zip"), 'w', compression=zipfile.ZIP_DEFLATED) as zip_ref:
-        zip_ref.write(output_file, arcname=output_file.name)
-
-    # Delete the uncompressed file
-    output_file.unlink()
+    save(results, output_file)
 
     print("Done!")
